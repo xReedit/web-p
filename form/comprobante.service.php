1f430fad9ce19606501fa1ee4b112aca6162061c
Marcelo <xReedit@gmail.com>
2019-02-09 13:47:27 -0500

    update 2.1

--- form/comprobante.service.php
+++ form/comprobante.service.php
@@ -4,7 +4,7 @@ $recaptcha = $_POST["g-recaptcha-response"];
 
     $url = 'https://www.google.com/recaptcha/api/siteverify';
     $data = array(
-        'secret' => '6LcFWZAUAAAAANPMp17k1KAPbNzscutJiF5knaKa',
+        'secret' => '6LcQWZAUAAAAAEii_1ILNF4FAnIO-lYSFsm4F8ET',
         'response' => $recaptcha
     );
 
