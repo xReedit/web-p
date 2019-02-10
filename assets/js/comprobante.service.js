cc3e306534b7885d35a78062088ee5ad2ccb353c
Marcelo <xReedit@gmail.com>
2019-02-10 17:03:02 -0500

    consultas

--- assets/js/comprobante.service.js
+++ assets/js/comprobante.service.js
@@ -1,13 +1,24 @@
-grecaptcha.ready(function () {
-	grecaptcha.execute('6LeRWZAUAAAAAHZOZ-Ezg7OUEbPjDa3jJncjU1o5', { action: 'homepage' })
-	.then(function (token) {						
-		document.getElementById('g-recaptcha-response').value = token;
-	});
+var dataLinks = {};
+
+
+
+grecaptcha.ready(function () {	
+	iniCaptcha();
+});
+
+$("#contact-form").submit(function(event) {	
+	if (!event.target.checkValidity()) return;	
+	xRetornaMoneda($("importe"));
+	consultar();
+	event.preventDefault();
 });
 
 function consultar() {
 	const e_res = $("#txt_res");
 	const formData = new FormData(document.getElementById("contact-form"));	
+
+	btn_consultar.disabled = true;
+	$(btn_consultar).text("Cargando ...");	
 
 	e_res.addClass("xInvisible");
 	fetch("../../../form/comprobante.service.php", {
@@ -15,13 +26,67 @@ function consultar() {
 		body: formData
 		}).then(function (response) {
 			return response.json();
-		}).then(function (res) {
+		}).then(function (res) {			
+			console.log(res);
 			if (res.res) { // si todo va bien
-				e_res.text(res.msj);
-			} else {				
+				const _data = JSON.parse(res.data);
+				if (_data.success) {
+					// muestra los iconos
+					const html_btns =
+						"<span>Click para descargar</span><br>" +
+						'<div class="p-10 btn btn-file" data-type="pdf" onclick="xDownloadFile(this)">' +
+						'<img src="images/001-pdf.png" alt="">' +
+						"</div>" +
+						'<div class="p-10 btn btn-file" data-type="xml" onclick="xDownloadFile(this)">' +
+						'<img src="images/002-xml.png" alt="">' +
+						"<div>";
+								
+					
+					$("#_btn_send").addClass('xInvisible');
+					$("#_btn_res").html(html_btns).trigger('create');
+
+					dataLinks = _data.links;
+
+				} else {
+					iniCaptcha();
+					e_res.removeClass("xInvisible");
+					e_res.text('No se encontro ningun comprobante.');		
+				}
+			} else {
+				iniCaptcha();			
 				e_res.removeClass("xInvisible");
 				e_res.text(res.msj);				
 			}
-    });
+
+			$(btn_consultar).text('Consultar');
+			btn_consultar.disabled = false;
+		}).catch(function (err) {
+			console.log(err);
+			iniCaptcha();
+			e_res.removeClass("xInvisible");
+			e_res.text('No se pudo conectar.');	
+		});;
 		
 }
+
+function iniCaptcha() {
+	grecaptcha.execute('6LeRWZAUAAAAAHZOZ-Ezg7OUEbPjDa3jJncjU1o5', { action: 'homepage' })
+		.then(function (token) {
+			document.getElementById('g-recaptcha-response').value = token;
+		});
+}
+
+function xDownloadFile(obj) {
+	const tipo = $(obj).attr('data-type');
+	const _url = tipo === 'pdf' ? dataLinks.pdf : dataLinks.xml;
+	window.open(_url, "_blank");
+}
+
+
+function xRetornaMoneda(xObj) {
+	var xVal = parseFloat($(xObj).val());
+	if (isNaN(xVal)) { xVal = 0; }
+	xObj.value = parseFloat(xVal).toFixed(2);
+}
+
+function conMayusculas(field) { field.value = field.value.toUpperCase(); };
--- form/comprobante.service.php
+++ form/comprobante.service.php
@@ -16,30 +16,77 @@ if($_POST){
         $Return = json_decode($Response);
         return $Return;
     }
-    $Return = getCaptcha($_POST['g-recaptcha-response']);
-    //var_dump($Return);
-    if($Return->success == true && $Return->score > 0.5){
-        $msj="Succes!";
-        $res=true;
-		// comprobar comprobantes
+    $Return = getCaptcha($_POST['g-recaptcha-response']);        
+    if($Return->success == true && $Return->score > 0.5){               
+    // if($Return->success == true){
+        
+
+        $numComprobante = explode("-", $_POST['num_comprobante']);        
+        $data = array(
+            "number_ruc_company" => $_POST['ruc_establecimiento'],
+            "series" => $numComprobante[0],
+            "number_document" => (int)$numComprobante[1],
+            "total" => $_POST['importe']
+        );
+
+        $_data=json_encode($data);
+
+        sendPost($_data);
+        
     }else{
         $msj="Eres un robot!";
         $res=false;
-        // print "Eres un robot";
-    }
-
-    responder($res,$msj);
-    
-    
-
-	
+        responder($res,$msj,'');        
+    }	
 }
 
+function sendPost($jsonDataEncoded)
+{    
+    
+    // return;
 
-function responder($res, $msj){
+    $res='';
+    $msj='';
+    //url contra la que atacamos
+    // $ch = curl_init("http://3.16.166.249/api/documents/consult_id");
+    $ch = curl_init("http://apifacturalo_a.test:8080/api/documents/getLinks");    
+    //a true, obtendremos una respuesta de la url, en otro caso, 
+    //true si es correcto, false si no lo es
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    // curl_setopt($ch, CURLOPT_HTTPHEADER,array("Content-type: application/json"));
+    //establecemos el verbo http que queremos utilizar para la petición
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    
+    //Attach our encoded JSON string to the POST fields.
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+ 
+    //Set the content type to application/json
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
+    
+    //enviamos el array data
+    // curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
+    //obtenemos la respuesta
+    $response = curl_exec($ch);
+    // Se cierra el recurso CURL y se liberan los recursos del sistema
+    curl_close($ch);
+    
+    if(!$response) {
+        $res=false;
+        $msj='No se encontro el comprobante';
+    }else{
+        $res=true;        
+    }
+
+    // $msj=json_encode($data); 
+
+    responder($res, $msj, $response);
+}
+
+function responder($res, $msj, $data){
     $response = array(
         'res' => $res,
-        'msj'=> $msj
+        'msj'=> $msj,
+        'data'=> $data
     );
 
     echo json_encode($response); 
