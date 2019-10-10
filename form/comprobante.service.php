08bb27c590d182f5a1a80824971e5e6ed502f194
marcelo <xreedit@gmail.com>
2019-10-10 01:11:45 -0500

    api2

--- form/comprobante.service.php
+++ form/comprobante.service.php
@@ -31,7 +31,7 @@ if($_POST){
 
         $_data=json_encode($data);
 
-        sendPost($_data);
+        sendPostApi2($_data);
         
     }else{
         $msj="No se encontro el comprobante...(r)";
@@ -93,7 +93,7 @@ function sendPostApi2($jsonDataEncoded)
     $msj='';
     //url contra la que atacamos
     // $ch = curl_init("http://3.16.166.249/api/documents/consult_id");
-    $chApi = curl_init("https://apifac2.papaya.com.pe/api/documents/getLinks");    
+    $chApi = curl_init("http://18.217.212.172/api/documents/getLinks");    
     //a true, obtendremos una respuesta de la url, en otro caso, 
     //true si es correcto, false si no lo es
     curl_setopt($chApi, CURLOPT_RETURNTRANSFER, true);
